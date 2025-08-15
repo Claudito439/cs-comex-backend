bbaf70524c648d0718b86b3a67ac9c0e6840e702
claudio174 <claudio.vasquez@believesol.com>
2025-08-14 23:14:00 -0400

    add logs

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    console.log('Conectando a MongoDB...');
+    console.log(process.env.MONGODB_URI);
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       // Opciones de conexión recomendadas
     });
